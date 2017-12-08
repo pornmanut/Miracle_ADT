f181537edec517bafa35a2a7ebe77cb1f06e8fd5
pornmanut <pornmanuty@gmail.com>
2017-12-08 20:57:07 +0700

    add prepend

--- just_LinkedLists.c
+++ just_LinkedLists.c
@@ -2,15 +2,46 @@
 #include <stdlib.h>
 
 typedef struct node{
+	
 	int data;
-	int node* next;
+	struct node* next;
+
 }NODE;
 
+NODE* create(int data,NODE* next){
+	
+	NODE* node = (NODE*)malloc(sizeof(NODE));
+	
+	node->data = data;
+	node->next = next;
+	
+	return node;
+}
 
+NODE* prepend(NODE* head,int data){
+	
+	NODE* node = create(data,head);
+	head = node;
+	return head;
 
+}
 
-
+void printInfo(NODE* head){
+	
+	NODE* cursor = head;
+	int count = 0;
+	while(cursor != NULL){
+		printf("%d: %d \n",count+1,cursor->data);
+		count++;
+		cursor = cursor->next;
+	}
+	printf("Count: %d\n",count);
+}
 int main(){
 	
+	NODE* start = NULL;
+	start = prepend(start,5);
+	start = prepend(start,6);
+	printInfo(start);
 	return 0;
 }
